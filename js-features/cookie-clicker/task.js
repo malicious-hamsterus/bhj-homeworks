387024d46038a228ab06a49befd2bb3207ed36fd
roman shesternin <malicious.hamsterus@gmail.com>
2021-08-09 23:24:58 +0600

    Работает кликер

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,14 @@
+const cookie = document.querySelector('#cookie')
+let counter = document.querySelector('#clicker__counter')
+let speed = document.querySelector('#clicker__timer')
+let dateLastClick = Date.now()
+
+cookie.addEventListener('click', function(event) {
+    counter.textContent ++
+    cookie.style.width = counter.textContent % 2 ? '220px' : '200px'
+    
+    time = Date.now()
+    console.log((time - dateLastClick) / 1000)
+    speed.textContent = Math.round(1000 / (time - dateLastClick) * 100) / 100
+    dateLastClick = time
+})
